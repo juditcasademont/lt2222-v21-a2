8aa1b5dfcf815513cd0b7a020b66c2def942f555
juditcasademont <75248254+juditcasademont@users.noreply.github.com>
2021-03-10 01:09:58 +0100

    Assignment submission

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 
 Assignment 2 from the course LT2222 in the University of Gothenburg's winter 2021 semester.
 
-Your name: *here*
+Your name: Judit Casademont Moner (guscasaju)
 
-*Answer all questions in the notebook here.  You should also write whatever high-level documentation you feel you need to here.*
+*Answer all questions in the notebook here. You should also write whatever high-level documentation you feel you need to here.*
+
+## Comments about decisions made throughout the code
+
+**Part 1**
+I chose to remove all punctuation because it might interfere at certain points in future functions.
+I realized that, in order for verbs to be lemmatized properly, I had to specify it by using the pos tags.
+
+**Part 2**
+I decided to include words with entity type tags in the context. My reason is that, for example, if we have a sentence that says "Carl XVI Gustaf, king of Sweden (...)", I think Sweden is a relevant word that defines who Carl XVI Gustaf is. In the same way, Carl XVI Gustaf is part of the definition of Sweden as well.
+
+**Part 3**
+Just as a comment, I found out that by creating the DataFrame from lists of numbers (as I had done in assignment 1) would give me an error on the test_y[0] cell but, instead, if I built it from an empty DataFrame, it worked. However, depending on the method I used to rearrange the columns, that cell would give me an error anyways. I haven't been able to figure out why that happened yet, still a mystery.
+
+**Part 5 COMMENT**
+I would say that the results of both the training data and the test data are pretty bad. I do not see much of a difference between the performances of the predictions in the two sets. Both have a strange tendency of guessing most of the instances as 'geo', 'gpe' and 'nat'. It is possible that we might have more examples of these entity types in the corpus than of the others, so these tags get more contexts. In the same way, if some of the entity types have very few examples in the corpus, the algorithm will see a lot less examples of them and will be unable to identify them as easily.
+My speculation is that my bad results might have to do with my decision in part 2 of including words with entity types in the contexts. By not stopping the gathering of features when a new entity is found, contexts might overlap and get mixed up. So my guess is that, maybe, a named entity being part of the context of another named entity creates a "conflict of identity". Another reason for these results might be my decision of not removing filler words or words that are very common (such as 'a', 'the', 'this', 'that', 'what', etc.), since these words are so common that they can appear in any context, therefore making the task of learning a typical contexts for the named entities a very complicated task.
+If I were to start the assignment all over again, I would for sure remove the named entities from the contexts of other named entities, as well as a collection of very common words.
--- a2.py
+++ a2.py
@@ -1,3 +1,4 @@
+# OG imports
 import sys
 import os
 import numpy as np
@@ -5,8 +6,15 @@ import numpy.random as npr
 import pandas as pd
 import random
 
+# my imports
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet as wn
+import re
+import string
+from sklearn.metrics import confusion_matrix as c_m
+
 # Module file with functions that you fill in so that they can be
-# called by the notebook.  This file should be in the same
+# called by the notebook. This file should be in the same
 # directory/folder as the notebook when you test with the notebook.
 
 # You can modify anything here, add "helper" functions, more imports,
@@ -16,7 +24,26 @@ import random
 
 # Function for Part 1
 def preprocess(inputfile):
-    return inputfile.readlines()
+
+    lemmatizer = WordNetLemmatizer()
+    verbs = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+
+    rows = []
+    line = [x.strip().split('\t') for x in inputfile.readlines()[1:]]
+    for l in line:
+        if l[3] not in verbs:
+            if l[2] not in string.punctuation:
+                l[2] = lemmatizer.lemmatize(l[2].lower())
+                rows.append(l)
+        else:
+            l[2] = wn._morphy(l[2].lower(), pos = 'v')
+            lemmatized = str(l[2]).strip("['").strip("']")
+            l[2] = lemmatized
+            rows.append(l)
+
+    cols = ["word nº", "sentence nº", "word", "POS tag", "entity type"]
+
+    return pd.DataFrame(rows, columns = cols)
 
 # Code for part 2
 class Instance:
@@ -31,39 +58,138 @@ class Instance:
         return str(self)
 
 def create_instances(data):
+    
     instances = []
-    for i in range(100):
-        neclass = random.choice(['art','eve','geo','gpe','nat','org','per','tim'])
-        features = ["something"] * random.randint(1, 10)
-        instances.append(Instance(neclass, features))
+    sent_num = list(data['sentence nº'])
+    word = list(data['word'])
+    ent_typ = list(data['entity type'])
+
+    sent_word_ent = list(zip(sent_num, word, ent_typ))
+
+    for tup in sent_word_ent:
+        if 'B-' in tup[2]: #If it has a tag with B-
+            neclass = re.sub('B-', '', tup[2])
+            features = []
+            start_token = '<start>'
+            end_token = '<end>'
+            befores = []
+            for c_before in range((sent_word_ent.index(tup) - 6),(sent_word_ent.index(tup) - 1)): #The 5 features before the word with a NE
+                if sent_word_ent[c_before][0] == tup[0]: #If they belong in the same sentence
+                    befores.append(sent_word_ent[c_before][1]) #Append the word
+                else:
+                    befores.append(start_token)
+            features = features + befores
+            afters = []
+            i = 1
+            j = 6
+            if (sent_word_ent.index(tup) + j) < len(sent_word_ent):
+                for c_after in range((sent_word_ent.index(tup) + i),(sent_word_ent.index(tup) + j)):
+                    if sent_word_ent[c_after][0] == tup[0]:
+                        afters.append(sent_word_ent[c_after][1])
+                    else:
+                        afters.append(end_token)
+            features = features + afters
+
+            instances.append(Instance(neclass, features))
     return instances
 
+from collections import Counter
 # Code for part 3
 def create_table(instances):
-    df = pd.DataFrame()
-    df['class'] = [random.choice(['art','eve','geo','gpe','nat','org','per','tim']) for i in range(100)]
-    for i in range(3000):
-        df[i] = npr.random(100)
 
-    return df
+    vocabulary = []
+    for instance in instances:
+        for feature in instance.features:
+            if feature not in vocabulary:
+                vocabulary.append(feature)
+    classes = []
+    for instance in instances:
+        classes.append(instance.neclass)
+    
+    empty_matrix = np.zeros((len(instances), len(vocabulary)))
+    table = pd.DataFrame(empty_matrix, columns = vocabulary)
+
+    i = 0
+    for instance in instances:
+        for context in instance.features:
+            table.loc[i, context] += 1
+        i +=1
+    
+    # new_order = ['class'] + vocabulary
+    # table = table.reindex(new_order, axis=1)
+
+    table['class'] = classes
+    cols = list(table.columns.values)
+    table = table[[cols[-1]]+cols[0:-2]]
+
+    # table.insert(0, "class", classes, True) 
+
+    # classes_df = pd.DataFrame(classes, columns = ['classes'])
+    # table = classes_df.join(table)
+
+
+    return table
+
+    """
+    Leaving this here in memory of a DataFrame that technically worked but
+    made my test functions have a bunch of errors for some reason I don't know.
+    I would very much appreciate to know the reason why it doesn't work
+    because I spent hours trying to figure it out and I couldn't :(
+    So I had to find a new way to make the matrix, and found out that by
+    starting with an empty matrix the train and test functions worked.
+    It's such a mystery, really *_*
+    """
+    # vocabulary = []
+    # for instance in instances:
+    #     for feature in instance.features:
+    #         # if feature not in vocabulary:
+    #         vocabulary.append(feature)
+
+    # tags = []
+    # for instance in instances:
+    #     tags.append(instance.neclass)
+
+    # # c = ['class']
+    # # columns = c + vocabulary
+
+    # contexts = []
+    # for instance in instances:
+    #     contexts.append([instance.features])
+    
+    # matrix = []
+    # tags_vocab = list(zip(tags, contexts))
+    # for tag, con in tags_vocab:
+    #     for w in con:
+    #         vec = [w.count(word) for word in tops]
+    #         row = [tag] + vec
+    #         matrix.append(row)
+
+    # return pd.DataFrame(matrix, columns = columns)
 
 def ttsplit(bigdf):
-    df_train = pd.DataFrame()
-    df_train['class'] = [random.choice(['art','eve','geo','gpe','nat','org','per','tim']) for i in range(80)]
-    for i in range(3000):
-        df_train[i] = npr.random(80)
 
-    df_test = pd.DataFrame()
-    df_test['class'] = [random.choice(['art','eve','geo','gpe','nat','org','per','tim']) for i in range(20)]
-    for i in range(3000):
-        df_test[i] = npr.random(20)
+    df_train = bigdf.sample(frac = 0.8)
+    df_test = bigdf.drop(df_train.index)
+
+    df_train = df_train.reset_index()
+    df_test = df_test.reset_index()
         
     return df_train.drop('class', axis=1).to_numpy(), df_train['class'], df_test.drop('class', axis=1).to_numpy(), df_test['class']
 
 # Code for part 5
 def confusion_matrix(truth, predictions):
-    print("I'm confusing.")
-    return "I'm confused."
+    
+    # types = ['art', 'eve', 'geo', 'gpe', 'nat', 'org', 'per', 'tim']
+    # confusion = pd.DataFrame(confusion_matrix(truth, predictions, labels = types))
+    # confusion.columns = types
+    # confusion.index = types
+
+    classes = ['art', 'eve', 'geo', 'gpe', 'nat', 'org', 'per', 'tim']
+    confusion = pd.DataFrame(c_m(truth, predictions, labels = classes))
+    confusion.index = classes
+    confusion.columns = classes
+
+    return confusion
 
 # Code for bonus part B
 def bonusb(filename):
